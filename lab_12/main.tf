d4b47eaced9346b2a64398151f92a66e110999a9
Yaksh Rawal <166778073+yaksh0210@users.noreply.github.com>
2024-04-25 12:15:05 +0530

    Delete lab_12 directory

--- lab_12/main.tf
+++
@@ -1,5 +0,0 @@
-resource "local_sensitive_file" "name" {
-    filename = var.users[count.index]
-    content = var.content
-    count = length(var.users)
-}
--- lab_12/variables.tf
+++
@@ -1,7 +0,0 @@
-variable "users" {
-    type = list
-}
-variable "content" {
-    default = "password: S3cr3tP@ssw0rd"
-
-}
